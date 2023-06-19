cb1eb3c2608a2a87eeb2aa1f0c8f9f49acb9afec
Gavin Crowley <gavincrowley@gmail.com>
2023-06-19 19:54:17 +0100

    pipline complete

---
+++ BIInsights/1.ETL-Py-PG/1.createdb.sql
@@ -0,0 +1,12 @@
+-- Database: AdventureWorks
+
+-- DROP DATABASE "AdventureWorks";
+
+CREATE DATABASE "AdventureWorks"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Ireland.1252'
+    LC_CTYPE = 'English_Ireland.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
---
+++ BIInsights/1.ETL-Py-PG/1stproj.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE "AdventureWorks"
+	WITH
+	OWNER = postgres
+	ENCODING = 'UTF8'
+	LC_COLLATE = 'English_Ireland.1252'
+	LC_CTYPE = 'English_Ireland.1252'
+	TABLESPACE = pg_default
+	CONNECTION LIMIT = -1
